aa81ec78301f5e55f835b67641ba5e4851567a33
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-01 00:48:49 +0000

    Split out logic for calculating successful payment amounts

--- models/marts/core/fct_orders.sql
+++ models/marts/core/fct_orders.sql
@@ -15,15 +15,26 @@ payments as (
 successful_payments as (
 
     select
+        payment_id,
         order_id,
-        sum(amount) as amount
+        status,
+        amount
 
     from payments
 
     where status = 'success'
 
-    group by
-        1
+),
+
+successful_payment_amounts as (
+
+    select
+        order_id,
+        sum(amount) as amount
+
+    from successful_payments
+
+    group by 1
 
 ),
 
@@ -32,12 +43,12 @@ final as (
     select
         orders.order_id,
         orders.customer_id,
-        successful_payments.amount
+        successful_payment_amounts.amount
 
     from orders
 
-    left join successful_payments
-        on orders.order_id = successful_payments.order_id
+    left join successful_payment_amounts
+        on orders.order_id = successful_payment_amounts.order_id
 
 )
 
