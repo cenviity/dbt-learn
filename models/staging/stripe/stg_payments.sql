f67ba219ee9de79fd1167dbdb7cdccea371360cf
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-01 01:27:10 +0000

    Convert cents to dollars

--- models/staging/stripe/stg_payments.sql
+++ models/staging/stripe/stg_payments.sql
@@ -7,9 +7,11 @@ payments as (
         orderid as order_id,
         paymentmethod as payment_method,
         status,
-        amount,
         created as created_date,
-        _batched_at
+        _batched_at,
+
+        -- Convert from cents to dollars
+        amount / 100 as amount
 
     from raw.stripe.payment
 
