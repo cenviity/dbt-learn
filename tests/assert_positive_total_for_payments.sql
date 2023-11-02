07b911b6d9dcab5d83354882bd6f7bee8e5528c8
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-02 01:17:58 +0000

    Add test for positive total payment amounts

---
+++ tests/assert_positive_total_for_payments.sql
@@ -0,0 +1,19 @@
+-- Refunds have a negative amount, so the total amount should always be >= 0.
+-- Therefore, return records where this isn't true to make the test fail.
+
+with
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+)
+
+select
+    order_id,
+    sum(amount) as total_amount
+
+from payments
+
+group by 1
+having total_amount < 0
