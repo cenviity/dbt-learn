a927ed70bd7d09123a2da38956f61e63f417b33f
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-22 06:06:03 +0000

    Refactor pivoting code with Jinja

--- models/marts/core/int_orders__pivoted.sql
+++ models/marts/core/int_orders__pivoted.sql
@@ -10,10 +10,24 @@ pivoted as (
 
     select
         order_id,
-        sum(case when payment_method = 'bank_transfer' then amount else 0 end) as bank_transfer_amount,
-        sum(case when payment_method = 'coupon' then amount else 0 end) as coupon_amount,
-        sum(case when payment_method = 'credit_card' then amount else 0 end) as credit_card_amount,
-        sum(case when payment_method = 'gift_card' then amount else 0 end) as gift_card_amount
+
+        {%- set payment_methods = ['bank_transfer', 'coupon', 'credit_card', 'gift_card'] -%}
+
+        {% for payment_method in payment_methods %}
+
+            sum(
+                case
+                    when payment_method = '{{ payment_method }}'
+                        then amount
+                    else 0
+                end
+            )
+                as {{ payment_method }}_amount
+            {%- if not loop.last -%}
+                ,
+            {%- endif -%}
+
+        {%- endfor %}
 
     from payments
 
