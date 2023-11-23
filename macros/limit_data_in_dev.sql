1e0b340caeddfec02e3019dfedfc4b234a6e4a67
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-23 06:07:27 +0000

    Remove extraneous space

--- macros/limit_data_in_dev.sql
+++ macros/limit_data_in_dev.sql
@@ -2,7 +2,7 @@
 
     {% if target.name == 'default' -%}
         
-        where {{ column_name }} >= dateadd('day', - {{ dev_days_of_data }}, current_timestamp)
+        where {{ column_name }} >= dateadd('day', -{{ dev_days_of_data }}, current_timestamp)
 
     {%- endif %}
 
