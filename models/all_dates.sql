0134f15b42ae0c85a031bfe31e60e87164e7d450
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-24 02:29:33 +0000

    Fix whitespace

--- models/all_dates.sql
+++ models/all_dates.sql
@@ -4,9 +4,10 @@
     )
 }}
 
-{{ dbt_utils.date_spine(
-    datepart="day",
-    start_date="cast('2020-01-01' as date)",
-    end_date="cast('2021-01-01' as date)"
-   )
+{{
+    dbt_utils.date_spine(
+        datepart="day",
+        start_date="cast('2020-01-01' as date)",
+        end_date="cast('2021-01-01' as date)"
+    )
 }}
