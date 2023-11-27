5db4869b24360cec16cf53b28493fd9459d339a3
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-27 03:38:28 +0000

    Add `dry_run` parameter

--- macros/clean_stale_models.sql
+++ macros/clean_stale_models.sql
@@ -7,7 +7,7 @@
 
 #}
 
-{% macro clean_stale_models(database=target.database, schema=target.schema, days=7) %}
+{% macro clean_stale_models(database=target.database, schema=target.schema, days=7, dry_run=True) %}
     
     {% set get_drop_commands_query %}
 
@@ -32,9 +32,18 @@
     {% set drop_queries = run_query(get_drop_commands_query).columns[1].values() %}
 
     {% for query in drop_queries %}
+
+        {% if dry_run %}
+            
+            {{ log(query, info=True) }}
         
-        {{ log(query, info=True) }}
-        
+        {% else %}
+
+            {{ log('Dropping object with command: ' ~ query, info=True) }}
+            {% do run_query(query) %}
+
+        {% endif %}
+
     {% endfor %}
 
 {% endmacro %}
