4ebc4d35b76b18f39b764d4a0eeb2384068be252
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-28 05:25:56 +0000

    Add `total_revenue` analysis

---
+++ analyses/total_revenue.sql
@@ -0,0 +1,25 @@
+with
+
+payments as (
+
+    select * from {{ ref('stg_payments') }}
+
+),
+
+successful_payments as (
+
+    select *
+    
+    from payments
+
+    where status = 'success'
+
+),
+
+total_revenue as (
+
+    select sum(amount) from successful_payments
+
+)
+
+select * from total_revenue
