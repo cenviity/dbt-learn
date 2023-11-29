fc3d015e4eca7591a0c4986b3078ce1e37d47103
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-30 09:11:28 +1100

    Add `customer_` prefix to fields and update `final` CTE

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -12,8 +12,6 @@ customers as (
 
 ),
 
---
-
 customer_orders as (
 
     select
@@ -26,23 +24,23 @@ customer_orders as (
 
         min(orders.order_date) over(
             partition by orders.customer_id
-        ) as first_order_date,
+        ) as customer_first_order_date,
 
         min(valid_order_date) over(
             partition by orders.customer_id
-        ) as first_non_returned_order_date,
+        ) as customer_first_non_returned_order_date,
 
         max(valid_order_date) over(
             partition by orders.customer_id
-        ) as most_recent_non_returned_order_date,
+        ) as customer_most_recent_non_returned_order_date,
 
         count(*) over(
             partition by orders.customer_id
-        ) as order_count,
+        ) as customer_order_count,
 
         sum(nvl2(orders.valid_order_date, 1, 0)) over(
             partition by orders.customer_id
-        ) as non_returned_order_count,
+        ) as customer_non_returned_order_count,
 
         sum(
             nvl2(
@@ -52,11 +50,11 @@ customer_orders as (
             )
         ) over(
             partition by orders.customer_id
-        ) as total_lifetime_value,
+        ) as customer_total_lifetime_value,
 
         array_agg(distinct orders.order_id) over(
             partition by orders.customer_id
-        ) as order_ids
+        ) as customer_order_ids
 
     from orders
 
@@ -69,85 +67,29 @@ add_avg_order_values as (
 
     select
         *,
-        total_lifetime_value / non_returned_order_count as avg_non_returned_order_value
+
+        customer_total_lifetime_value / customer_non_returned_order_count
+            as customer_avg_non_returned_order_value
 
     from customer_orders
-
-),
-
---
-
-customer_order_history as (
-
-    select
-        -- customers.customer_id,
-        -- customers.full_name,
-        -- customers.surname,
-        -- customers.givenname,
-
-        min(orders.order_date) as first_order_date,
-
-        min(valid_order_date) as first_non_returned_order_date,
-
-        max(valid_order_date) as most_recent_non_returned_order_date,
-
-        coalesce(max(orders.user_order_seq),0) as order_count,
-
-        coalesce(count(case
-            when orders.valid_order_date is not null
-            then 1
-        end),0) as non_returned_order_count,
-
-        sum(case
-            when orders.valid_order_date is not null
-            then orders.order_value_dollars
-            else 0
-        end) as total_lifetime_value,
-
-        sum(case
-            when orders.valid_order_date is not null
-            then orders.order_value_dollars
-            else 0
-        end)
-        / nullif(count(case
-            when orders.valid_order_date is not null
-            then 1
-        end),0) as avg_non_returned_order_value,
-
-        array_agg(distinct orders.order_id) as order_ids
-
-    from orders
-
-    join customers
-    on orders.customer_id = customers.customer_id
-
-    where orders.order_status not in ('pending')
-
-    group by customers.customer_id, customers.full_name, customers.surname, customers.givenname
 
 ),
 
 final as (
 
     select
-        orders.order_id,
-        orders.customer_id,
-        customers.surname,
-        customers.givenname,
-        first_order_date,
-        order_count,
-        total_lifetime_value,
-        orders.order_value_dollars,
-        orders.order_status,
-        orders.payment_status
+        order_id,
+        customer_id,
+        surname,
+        givenname,
+        customer_first_order_date as first_order_date,
+        customer_order_count as order_count,
+        customer_total_lifetime_value as total_lifetime_value,
+        order_value_dollars,
+        order_status,
+        payment_status
 
-    from orders
-
-    join customers
-    on orders.customer_id = customers.customer_id
-
-    join customer_order_history
-    on orders.customer_id = customer_order_history.customer_id
+    from add_avg_order_values
 
 )
 
