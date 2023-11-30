6908d6b6f6e301c1a377db3444efb9989d37e8e4
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-30 14:51:03 +1100

    Use fully qualified references

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -6,7 +6,7 @@ orders as (
 
 ),
 
-payments as (
+base_payments as (
 
     select * from {{ source('stripe', 'payment') }}
 
@@ -18,7 +18,7 @@ customers as (
 
 ),
 
-p as (
+payments as (
 
     select
         orderid as order_id,
@@ -26,7 +26,7 @@ p as (
         max(created) as payment_finalized_date,
         sum(amount) / 100.0 as total_amount_paid
 
-    from payments
+    from base_payments
 
     where status != 'fail'
 
@@ -41,50 +41,52 @@ paid_orders as (
         orders.user_id as customer_id,
         orders.order_date as order_placed_at,
         orders.status as order_status,
-        p.total_amount_paid,
-        p.payment_finalized_date,
-        c.first_name as customer_first_name,
-        c.last_name as customer_last_name
+
+        payments.total_amount_paid,
+        payments.payment_finalized_date,
+
+        customers.first_name as customer_first_name,
+        customers.last_name as customer_last_name
 
     FROM orders
 
-    left join p
-        ON orders.id = p.order_id
+    left join payments
+        ON orders.id = payments.order_id
 
-    left join customers as c
-        on orders.user_id = c.id
+    left join customers
+        on orders.user_id = customers.id
 
 ),
 
 customer_orders as (
 
     select
-        c.id as customer_id,
+        customers.id as customer_id,
 
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
+        min(orders.order_date) as first_order_date,
+        max(orders.order_date) as most_recent_order_date,
         count(orders.id) as number_of_orders
 
-    from customers as c
+    from customers
 
     left join orders
-        on orders.user_id = c.id
+        on orders.user_id = customers.id
 
     group by 1
 
 ),
 
-x as (
+add_customer_lifetime_value as (
 
     select
-        p.order_id,
+        paid_orders.order_id,
 
-        sum(p.total_amount_paid) over (
-            partition by p.customer_id
+        sum(paid_orders.total_amount_paid) over (
+            partition by paid_orders.customer_id
             order by order_id
         ) as customer_lifetime_value
 
-    from paid_orders p
+    from paid_orders
 
     order by 1
 
@@ -93,30 +95,30 @@ x as (
 final as (
 
     select
-        p.*,
+        paid_orders.*,
 
-        row_number() over (order by p.order_id) as transaction_seq,
+        row_number() over (order by paid_orders.order_id) as transaction_seq,
 
         row_number() over (
-            partition by customer_id
-            order by p.order_id
+            partition by paid_orders.customer_id
+            order by paid_orders.order_id
         ) as customer_sales_seq,
 
-        case when c.first_order_date = p.order_placed_at
+        case when customer_orders.first_order_date = paid_orders.order_placed_at
             then 'new'
             else 'return'
         end as nvsr,
 
-        x.customer_lifetime_value,
-        c.first_order_date as fdos
+        add_customer_lifetime_value.customer_lifetime_value,
+        customer_orders.first_order_date as fdos
 
-        from paid_orders p
+        from paid_orders
 
-        left join customer_orders as c
+        left join customer_orders
             using (customer_id)
 
-        left join x
-            on x.order_id = p.order_id
+        left join add_customer_lifetime_value
+            on add_customer_lifetime_value.order_id = paid_orders.order_id
 
         order by order_id
 
