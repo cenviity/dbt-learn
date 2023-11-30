fa75e75595659bc7c0b9710a9e91181c95e30a0d
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-11-30 16:23:26 +1100

    Remove extraneous table references and list fields explicitly

--- models/marts/fct_customer_orders.sql
+++ models/marts/fct_customer_orders.sql
@@ -61,22 +61,31 @@ paid_orders as (
 final as (
 
     select
-        paid_orders.*,
+        order_id,
+        customer_id,
+        order_placed_at,
+        order_status,
+
+        total_amount_paid,
+        payment_finalized_date,
+
+        customer_first_name,
+        customer_last_name,
 
         -- Sales transaction sequence
-        row_number() over (order by paid_orders.order_id) as transaction_seq,
+        row_number() over (order by order_id) as transaction_seq,
 
         -- Customer sales sequence
         row_number() over (
-            partition by paid_orders.customer_id
-            order by paid_orders.order_id
+            partition by customer_id
+            order by order_id
         ) as customer_sales_seq,
 
         -- New vs returning customer
         case when (
             rank() over (
-                partition by paid_orders.customer_id
-                order by paid_orders.order_placed_at, paid_orders.order_id
+                partition by customer_id
+                order by order_placed_at, order_id
             ) = 1
         )
             then 'new'
@@ -84,15 +93,15 @@ final as (
         end as nvsr,
 
         -- Customer lifetime value
-        sum(paid_orders.total_amount_paid) over (
-            partition by paid_orders.customer_id
+        sum(total_amount_paid) over (
+            partition by customer_id
             order by order_id
         ) as customer_lifetime_value,
 
         -- First day of sale
-        first_value(paid_orders.order_placed_at) over (
-            partition by paid_orders.customer_id
-            order by paid_orders.order_placed_at
+        first_value(order_placed_at) over (
+            partition by customer_id
+            order by order_placed_at
         ) as fdos
 
         from paid_orders
