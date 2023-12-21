fa3b29c6ec6ed5f07b82383f3121cb57b2e96b11
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-21 14:33:45 +1100

    Add config to not require tests for model

--- models/marts/core/orders_by_customer_9.sql
+++ models/marts/core/orders_by_customer_9.sql
@@ -1,3 +1,11 @@
+-- This overrides the config in `dbt_project.yml`
+-- and this model will not require tests.
+{{
+  config(
+    required_tests = None,
+    )
+}}
+
 select
     customer_id,
     count(order_id) as num_orders
