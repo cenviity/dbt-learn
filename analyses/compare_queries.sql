62e8e2da3122b554d1ace0808e965c312996940e
Vincent Ging Ho Yim <cenviity@gmail.com>
2023-12-22 01:18:12 +1100

    Add analysis to compare `fct_orders` with `__deprecated` model

---
+++ analyses/compare_queries.sql
@@ -0,0 +1,9 @@
+{% set old_etl_relation=ref('fct_orders__deprecated') %}
+
+{% set dbt_relation=ref('fct_orders') %}
+
+{{ audit_helper.compare_relations(
+    a_relation=old_etl_relation,
+    b_relation=dbt_relation,
+    primary_key="order_id"
+) }}
